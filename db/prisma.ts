9c7f2d0c9aaa175bcba0568b6eae19dcbf3a3d7f
PremMann <37853531+PremMann@users.noreply.github.com>
2025-11-26 13:12:29 +0700

    testing
    testing connection

--- db/prisma.ts
+++ db/prisma.ts
@@ -10,6 +10,7 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined');
 }
 const connectionString = `${process.env.DATABASE_URL}`;
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 export const prisma = (() => {
   if (process.env.NODE_ENV === 'production') {
